644a5e3e6bbc295cba95559a143b80242f796a81
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-01-20 18:02:06 -0600

    Add sizeof.

--- macos/test.c
+++ macos/test.c
@@ -6,6 +6,8 @@ int main(int argc, char** argv) {
   time_t time;
   char buffer[80];
 
+  printf("sizeof time_t = %zu\n", sizeof(time_t));
+
   strptime("2038/01/19 03:14:07", "%Y/%m/%d %H:%M:%S", &tm);
   time = timegm(&tm);
   printf("time = 0x%08lx\n", time);
