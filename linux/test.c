8bac4ab12191c40c65b7198366832c8d43795de3
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-02-23 16:31:32 -0600

    Add error handling for linux.

--- linux/test.c
+++ linux/test.c
@@ -1,6 +1,7 @@
 #define _DEFAULT_SOURCE /* for timegm */
 #define _XOPEN_SOURCE /* for strptime */
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 int main(int argc, char** argv) {
@@ -10,19 +11,39 @@ int main(int argc, char** argv) {
 
   printf("sizeof time_t = %zu\n", sizeof(time_t));
 
-  strptime("2038/01/19 03:14:07", "%Y/%m/%d %H:%M:%S", &tm);
+  if (strptime("2038/01/19 03:14:07", "%Y/%m/%d %H:%M:%S", &tm) == NULL) {
+    printf("strptime error\n");
+    abort();
+  }
+
   time = timegm(&tm);
   printf("time = 0x%08lx\n", time);
 
-  gmtime_r(&time, &tm);
-  strftime(buffer, 80, "%Y/%m/%d %H:%M:%S", &tm);
+  if (gmtime_r(&time, &tm) == NULL) {
+    printf("gmtime_r error\n");
+    abort();
+  }
+
+  if (strftime(buffer, 80, "%Y/%m/%d %H:%M:%S", &tm) == 0) {
+    printf("strftime error\n");
+    abort();
+  }
+
   printf("time = %s\n", buffer);
 
   time += 1;
   printf("time + 1 = 0x%08lx\n", time);
 
-  gmtime_r(&time, &tm);
-  strftime(buffer, 80, "%Y/%m/%d %H:%M:%S", &tm);
+  if (gmtime_r(&time, &tm) == NULL) {
+    printf("gmtime_r error\n");
+    abort();
+  }
+
+  if (strftime(buffer, 80, "%Y/%m/%d %H:%M:%S", &tm) == 0) {
+    printf("strftime error\n");
+    abort();
+  }
+
   printf("time + 1 = %s\n", buffer);
 
   return 0;
